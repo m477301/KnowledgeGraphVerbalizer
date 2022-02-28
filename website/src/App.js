18c058fcea278f046dc88f74b6d6709ed8f564e0
Cianna MacMahon <ciannamacmahon@gmail.com>
2022-02-28 12:31:29 +0000

    Basic layout of User Interface

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
-import "./App.css";
+import Header from './Components/Header'
+import Button from './Components/Buttons'
+import imageOne from './A-knowledge-graph-structure.png'
 import { queryWikidataWithSparql } from "./Wikidata/APIWrapper";
 
 function App() {
@@ -10,7 +12,22 @@ function App() {
     }
     fetchData();
   }, []);
-  return <div className="App">Hello React!</div>;
+  return (
+    <div className='container'>
+      <div className='header'>
+        <Header/>
+        <img src={imageOne} className="image" alt></img>
+          <div className="button">
+             <Button text='Submit Knowledge Graph' >
+             </Button>
+               <div className='paragraph'>
+               <p>The verbalisation of the Knowledge Graph will appear here</p>
+              </div>
+          </div>  
+       </div>
+    </div> 
+  );
 }
 
 export default App;
+
---
+++ website/src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header= () =>{
+    return (
+        <header >
+            <h1>Hey Knowledge Graph, <br />
+            tell me something!</h1>          
+        </header>
+    )
+  }
+  export default Header
