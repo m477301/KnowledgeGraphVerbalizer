c13e095440a673bfe70bc6922cfd47fd5b78fdc5
Matteo McGuinness <matteo.mcguinness@gmail.com>
2022-03-21 10:48:48 +0000

    basicQuery

---
+++ website/src/Wikidata/APIWrapper.js
@@ -0,0 +1,27 @@
+// import { SparqlClient } from "sparql-http-client";
+const axios = require("axios");
+
+const endpointUrl = "https://query.wikidata.org/sparql";
+const sparqlQuery = `SELECT ?subject ?subjectLabel ?predicate ?predicate_label ?object ?objectLabel
+WHERE {
+  ?subject ?predicate ?object; wdt:P31 wd:Q5; wdt:P106 wd:Q937857.
+  VALUES ?subject { wd:Q21621995 wd:Q11571 wd:Q615 }
+  ?x wikibase:directClaim ?predicate; rdfs:label ?predicate_label. 
+  FILTER((LANG(?predicate_label)) = "en")
+  FILTER((((ISLITERAL(?object)) && (LANGMATCHES(LANG(?object), "en"))) || ((ISLITERAL(?object)) && (LANGMATCHES(LANG(?object), "")))) || (!(ISLITERAL(?object))))
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+} LIMIT 500 # Cutting the result stream after the first 500 lines.`;
+const url =
+  "https://query.wikidata.org/sparql?query=" + encodeURIComponent(sparqlQuery);
+const headers = { Accept: "application/sparql-results+json" };
+
+export const queryWikidataWithSparql = async () => {
+  let randomCity = "lala";
+  await axios
+    .get(url, { headers })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((e) => console.error(e));
+  return randomCity;
+};
