3ca6d0ff96b1e4c96532f271922d25286f6fe84e
Matteo McGuinness <matteo.mcguinness@gmail.com>
2022-03-21 13:46:52 +0000

    hello'

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
+import React from "react";
 import { useEffect } from "react";
-import Header from './Components/Header'
-import Button from './Components/Buttons'
-import imageOne from './A-knowledge-graph-structure.png'
+import "./App.css";
+import NavBar from "./Components/NavigationBar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/websitePages/Home";
+import StartQuery from "./Components/websitePages/Start";
+import About from "./Components/websitePages/About";
+import Contact from "./Components/websitePages/Contact";
 import { queryWikidataWithSparql } from "./Wikidata/APIWrapper";
-=======
-import React from 'react';
-import './App.css';
-import NavBar from './Components/NavigationBar/NavBar';
-import { BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-import Home from './Components/websitePages/Home';
-import StartQuery from './Components/websitePages/Start';
-import About from './Components/websitePages/About';
-import Contact from './Components/websitePages/Contact';
 
-import Button from './Components/Buttons';
-
->>>>>>> e473b90 (updated user interface)
+import Button from "./Components/Buttons";
 
 function App() {
   useEffect(() => {
@@ -29,22 +22,17 @@ function App() {
   return (
     <>
       <Router>
-      <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="/start"  element={<StartQuery/>} />
-          <Route path="/about"  element={<About/>} />
-          <Route path="/contact"  element={<Contact/>} />
-          <Route path="/start" element={<Home.Button/>}/>
-
-      </Routes>
-        
+          <Route path="/" exact element={<Home />} />
+          <Route path="/start" element={<StartQuery />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/start" element={<Home.Button />} />
+        </Routes>
       </Router>
-
     </>
-      
-      );
-    }
-   
-export default App;
+  );
+}
 
+export default App;
