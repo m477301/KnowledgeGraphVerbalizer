970963e83507f725b3f8b4c1c95273a90925e048
JASON WOOD <jawood@tcd.ie>
2022-03-22 19:55:36 +0000

    Basic dropdown  added to Get Started

--- website/src/Components/websitePages/Start.js
+++ website/src/Components/websitePages/Start.js
@@ -2,12 +2,24 @@ import React from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
-function StartQuery() {
-  return (
-    <div className='startQuery'>
-       <h1>Trial Query</h1>
-    </div>
-  );
+export default class DropdownMenu extends React.Component {
+  onChange(event) {
+    console.log("Dropdown Value Changed");
+    console.log("value:", event.currentTarget.value);
+  }
+  render() {
+    return (
+      <select onChange={this.onChange}>
+        <option value="Messi">
+          Lionel Messi
+        </option>
+        <option value="Kanye">
+          Kanye West
+        </option>
+        <option value="Brad Pitt">
+          Brad Pitt
+        </option>
+      </select>
+    );
+  }
 }
-
-export default StartQuery;
