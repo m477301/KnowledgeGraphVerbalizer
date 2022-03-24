fcff1933d3b4e1f742d771c0726a353ddc8a955f
Cianna MacMahon <ciannamacmahon@gmail.com>
2022-03-24 10:52:55 +0000

    included Jasons dropdown menu,added an output box to Get Started and attempted integration

--- website/src/App.js
+++ website/src/App.js
@@ -6,20 +6,11 @@ import Home from "./Components/websitePages/Home";
 import StartQuery from "./Components/websitePages/Start";
 import About from "./Components/websitePages/About";
 import Contact from "./Components/websitePages/Contact";
-import { queryWikidataWithSparql } from "./Wikidata/APIWrapper";
-import { useEffect } from "react";
-import { verbaliseSentences } from "./verblisation/tuto";
+
 import Button from "./Components/Buttons";
 
 function App() {
-  useEffect(() => {
-    async function fetchData() {
-      const lists = await queryWikidataWithSparql();
-      const sentences = verbaliseSentences(lists);
-      console.log(sentences);
-    }
-    fetchData();
-  }, []);
+  
   return (
     <>
       <Router>
--- website/src/Components/websitePages/Start.js
+++ website/src/Components/websitePages/Start.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import './Start.css';
+import { queryWikidataWithSparql } from "../../Wikidata/APIWrapper";
+import { useEffect } from "react";
+//import { verbaliseSentences } from "../../verblisation/tuto";
 
-function StartQuery() {
-  return (
-    <div className='startQuery'>
-       <h1>Trial Query</h1>
-    </div>
-  );
+export default class DropdownMenu extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={value:""};
+  }
+  /*
+  fetchData() {
+      const lists = await queryWikidataWithSparql();
+      const sentences = verbaliseSentences(lists);
+      console.log(sentences);
+  }
+  */
+    
+  onChange=event=> {
+    console.log("Dropdown Value Changed");
+    console.log("value:", event.currentTarget.value);
+    /*fetchData();*/
+    this.setState({
+      value:event.target.value});
+  };
+  render() {
+    return (
+      <section>
+      <label>
+        <h3>
+          Select your Query:  
+        </h3>
+        <select value={this.state.value}onChange={this.onChange}>
+        <option value="Messi Verbalisation ">Lionel Messi</option>
+        <option value="Ronaldo Verbalisation">Cristiano Ronaldo </option>
+      </select>
+      </label>
+
+      <div className='paragraph'>
+          <p> {this.state.value}</p>
+       
+      </div>
+      </section>
+    );
+  }
 }
-
-export default StartQuery;
