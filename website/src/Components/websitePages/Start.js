f984bec764cf183c40a89ff5792f2382c35fa8b5
Matteo McGuinness <matteo.mcguinness@gmail.com>
2022-03-28 21:08:28 +0100

    otherPlayers

--- website/src/Components/websitePages/Start.js
+++ website/src/Components/websitePages/Start.js
@@ -3,17 +3,18 @@ import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './Start.css';
+import "./Start.css";
 import { FaTruckLoading } from "react-icons/fa";
 function StartQuery() {
   const [paragraph, setParagraph] = useState(null);
-  const[textResults, setTextResults]=useState("Results will be shown here...");
-  
+  const [textResults, setTextResults] = useState(
+    "Results will be shown here..."
+  );
 
-  const showTextResults=(results) => setTextResults(results);
+  const showTextResults = (results) => setTextResults(results);
   useEffect(() => {
     async function fetchData() {
-      await axios.get("http://localhost:3500/verbalise/Mbappe").then((res) => {
+      await axios.get("http://localhost:3500/verbalise/Messi").then((res) => {
         setParagraph(res.data);
       });
     }
@@ -23,38 +24,36 @@ function StartQuery() {
   if (paragraph) {
     return (
       <>
-      <h2 className="title">  
-        Query selected: Lionel Messi
-    </h2>
-    <button type="button" className="resultsButton" onClick={()=>showTextResults(paragraph)}>
-      Click to see Results
-    </button>
+        <h2 className="title">Query selected: Lionel Messi</h2>
+        <button
+          type="button"
+          className="resultsButton"
+          onClick={() => showTextResults(paragraph)}
+        >
+          Click to see Results
+        </button>
         <div className="paragraphBox">
-        <p>{textResults}</p>
-    </div>
-
+          <p>{textResults}</p>
+        </div>
       </>
-    
     );
- }
+  }
 
   return (
-      <>
-      <h2 className="title">  
-        Query selected: Lionel Messi
-    </h2>
-    <button type="button" className="resultsButton" onClick={()=>showTextResults(paragraph)}>
-      Click to see Results
-    </button>
-        <div className="paragraphBox">
+    <>
+      <h2 className="title">Query selected: Lionel Messi</h2>
+      <button
+        type="button"
+        className="resultsButton"
+        onClick={() => showTextResults(paragraph)}
+      >
+        Click to see Results
+      </button>
+      <div className="paragraphBox">
         <p>Loading...</p>
-    </div>
-
-      </>
-    
-    );
-  
+      </div>
+    </>
+  );
 }
-
 
 export default StartQuery;
