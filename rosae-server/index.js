d70c4f50477ed4d57b697082312e8f761b64168d
Matteo McGuinness <matteo.mcguinness@gmail.com>
2022-04-14 13:13:20 +0100

    comments'

--- rosae-server/index.js
+++ rosae-server/index.js
@@ -1,15 +1,25 @@
+/* THIRD-PARTY FUNCTIONS */
 import express from "express";
 import cors from "cors";
-const app = express();
-app.use(cors());
-const port = 3500;
+
+/* HELPER FUNCTIONS */
 import { queryWikidataWithSparql } from "./Wikidata/ApiWrapper.js";
 import { verbaliseSentences } from "./tuto.js";
 
+/* Express API setup */
+const app = express();
+app.use(cors());
+const port = 3500;
+
+/* Test api endpoint */
 app.get("/", (req, res) => {
   res.send("Welcome to the Server!");
 });
 
+/* GET: Knowledge Graph Verbalisation Endpoint
+  parameter: query value
+  return: {trouples: <{[{subject, predicate, object}, ...]}>, sentences: <English Sentences>} 
+*/
 app.get("/verbalise/:name", async (req, res) => {
   const knowledgeGraph = await queryWikidataWithSparql(req.params.name);
   const sentences = await verbaliseSentences(knowledgeGraph);
@@ -22,6 +32,7 @@ app.get("/verbalise/:name", async (req, res) => {
   });
 });
 
+/* Setup and Start Server */
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
